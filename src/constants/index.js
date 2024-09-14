84e650195f0868d1c7437271d87ad776ca5cbb4f
FardadRahmani <rahmanifardad@yahoo.de>
2024-09-14 12:05:23 +0200

    second change

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,48 +3,48 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate Software developer with a knack for crafting robust and scalable applications. With 5 years of hands-on experience in the IT-Section, I have honed my skills and Understanding in the workings of modern enterprises. In the course of my retraining I am getting gradually more acquainted with different Programming Languages such as Python, C++ and Kotlin, as well as recent acquaintance into webdevelopment with React, Next.js as well as back-end technologies like Node.js, PostgreSQL. My goal is to leverage my expertise in Software Development, especially in relation to A.I.and machine Learning to create innovative solutions that drive business growth and so be a part of this great new hype that will in my opinion conquer every bit of our digital and normal life.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile Software developer with a passion for creating efficient and user-friendly applications. I have worked with a variety of technologies, including C++, C, Kotlin, Java and PostgreSQL, however what intrigued me the most and left me craving more was definitely PYTHON. My journey into Python-development began with a deep curiosity for how A.I. works, and even though I didnt take python serious at first as a programming Language, from the moment I started learning it my initial curiosity has evolved into a deep Connectedness where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2024 - Present",
+    role: "Software Developer Trainee",
+    company: "Macromedia Academy",
+    description: `Learning Software development especially OOP.`,
+    technologies: ["C++", "React.js", "Python", "PostgreSQL"],
   },
   {
     year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
+    role: "IT-Helpdesk",
+    company: "Staatliche Lotto-GmbH BW",
     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    technologies: ["incident management", "Software distribution", "ELO", "site management"],
   },
   {
     year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    role: "Senior Technician",
+    company: "Computacenter AG",
+    description: `Support of well-known end customers on-site.Maintenance of customer systems according to IMAC.`,
+    technologies: ["Software distribution", "tickets", "", ""],
   },
   {
     year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    role: "Imaging Process Specialist",
+    company: "Examion GmbH",
+    description: `Advanced imaging specialist training CT, MRI, X-Ray`,
+    technologies: ["XRAY imaging systems", "CT", "MRI", "X-Ray"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Weather App",
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["Kotlin", "Jetpack Compose", "Co-Routines", "Asynchronous Programming", "Composables"],
   },
   {
     title: "Task Management App",
@@ -61,16 +61,15 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
+    title: "PyQt Application",
     image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    description: "Desktop application developed using PyQt for GUI applications.",
+    technologies: ["Python", "PyQt"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Bad Cannstatt, Stuttgart ",
+  phoneNo: "+49-Ask Per Mail ",
+  email: "rahmanifardad@yahoo.de",
 };
