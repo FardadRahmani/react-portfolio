4e84ff5f1e6be5b2945513bc63c0b3c92aee300e
FardadRahmani <rahmanifardad@yahoo.de>
2024-09-17 11:39:15 +0200

    Remove Insta Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://github.com/FardadRahmani"><FaGithub /></a>
        
-        <a href="https://github.com/FardadRahmani"><FaInstagram /></a>
+        
       </div>
     </nav>
   )
